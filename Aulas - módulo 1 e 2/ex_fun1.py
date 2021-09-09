de2d222bb4501ea06ddca9a96eb980491c442893
Vinicius <vih-reis@hotmail.com>
2021-09-09 16:02:01 -0300

    att

---
+++ Aulas - módulo 1 e 2/ex_fun1.py
@@ -0,0 +1,19 @@
+def saudacao(a, b):
+    return print(a, b)
+
+
+saudacao('Olá', 'Vinicius')
+
+
+def soma(a, b, c):
+    return print(a + b + c)
+
+
+soma(2, 10, 15)
+
+
+def aumento(x, y):
+    return print(x + (x * (y / 100)))
+
+
+aumento(1500, 15)
