c51cee700ef8985765ee58e4e065ff3d13cb05c1
Vinicius <vih-reis@hotmail.com>
2021-09-10 12:17:08 -0300

    cont aulas

--- Aulas - módulo 1 e 2/ex_fun1.py
+++ Aulas - módulo 1 e 2/ex_fun1.py
@@ -1,19 +1,35 @@
 def saudacao(a, b):
-    return print(a, b)
+    return (a, b)
 
 
-saudacao('Olá', 'Vinicius')
+print(saudacao('Olá', 'Vinicius'))
 
 
 def soma(a, b, c):
-    return print(a + b + c)
+    return (a + b + c)
 
 
-soma(2, 10, 15)
+print(soma(2, 10, 15))
 
 
 def aumento(x, y):
-    return print(x + (x * (y / 100)))
+    return (x + (x * (y / 100)))
 
 
-aumento(1500, 15)
+print(aumento(1500, 15))
+
+
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'fizz'
+    elif n % 5 == 0:
+        return 'buzz'
+    return n
+
+
+print(fizzbuzz(15))
+print(fizzbuzz(5))
+print(fizzbuzz(3))
+print(fizzbuzz(7))
