b03d03ec23bbafc30fa72a1cf5b18b4a609eb821
Vinicius <vih-reis@hotmail.com>
2021-09-17 12:19:44 -0300

    conclusion ex somandolistas

---
+++ Aulas - módulo 3/ex_somando_listas.py
@@ -0,0 +1,15 @@
+lista_a = [1, 2, 3, 4, 5, 6]
+lista_b = [1, 2, 3, 4]
+lista_soma = []
+
+if len(lista_a) < len(lista_b):
+    for x in range(len(lista_a)):
+        lista_soma.append(lista_a[x] + lista_b[x])
+else:
+    for x in range(len(lista_b)):
+        lista_soma.append(lista_a[x] + lista_b[x])
+print(lista_soma)
+
+
+lista_total = [x + y for x, y in zip(lista_a, lista_b)]
+print(lista_total)
