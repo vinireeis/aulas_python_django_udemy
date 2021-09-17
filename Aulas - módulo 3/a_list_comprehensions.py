9c886dc1fc15ea526378aa670866113315138e7c
Vinicius <vih-reis@hotmail.com>
2021-09-17 10:47:13 -0300

    formating and exerc listcomprehensions1

--- Aulas - módulo 3/a_list_comprehensions.py
+++ Aulas - módulo 3/a_list_comprehensions.py
@@ -6,17 +6,20 @@ print(l2)
 exemplo2 = [v * 2 for v in l1]  # multiplicando cada elemento por 2
 print(exemplo2)
 
-exemplo3 = [(v, v2) for v in l1 for v2 in range(3)]  # iterando e criando tuplas num range 3
+exemplo3 = [(v, v2) for v in l1 for v2 in range(3)]
+# iterando e criando tuplas num range 3
 print(exemplo3)
+print('-' * 50)
 
 l11 = ['Vinicius', 'Larissa', 'Cintia']
-ex4 = [v.replace('i', 'e') for v in l11]  # ffazendo um replace numa lista
+ex4 = [v.replace('i', 'e') for v in l11]  # fazendo um replace numa lista
 print(ex4)
 
 tupla = (
     ('chave', 'valor'),
     ('chave2', 'valor2')
 )
+print('proximo é um dict?')
 ex5 = [(x, y) for y, x in tupla]  # convertendo uma tupla em um dict
 print(ex5)
 ex5 = dict(ex5)
@@ -24,8 +27,8 @@ print(ex5)
 l3 = list(range(100))
 ex6 = [v for v in l3 if v % 3 == 0 if v % 8 == 0]  # usando ifs
 print(ex6)
-ex7 = [v if v % 3 == 0 and v % 8 == 0 else 'Não é' for v in l3]  # if else
-print(ex7)
+#  ex7 = [v if v % 3 == 0 and v % 8 == 0 else 'Não é' for v in l3]  # if else
+#  print(ex7)
 print('-' * 50)
 
 lista = [
@@ -36,7 +39,8 @@ lista = [
 d1 = {x: y for x, y in lista}  # transformando em dicionário.
 print(d1)
 
-d2 = {x: y for x, y in enumerate(range(5))}  # usando o range para fazer um dicionário
+d2 = {x: y for x, y in enumerate(range(5))}
+# usando o range para fazer um dicionário
 print(d2)
 
 d3 = {x for x in range(5)}  # type é set
---
+++ Aulas - módulo 3/ex_listcomprehensions1.py
@@ -0,0 +1,14 @@
+carrinho = [
+    ('Produto1', 30),
+    ('Produto2', 20),
+    ('Produto3', 50),
+    ('Produto4', 99),
+    ('Produto5', 30),
+    ('Produto6', 50),
+    ('Produto7', 150),
+    ('Produto8', 10),
+
+]
+
+l1 = sum([(x[1]) for x in carrinho])
+print(l1)
