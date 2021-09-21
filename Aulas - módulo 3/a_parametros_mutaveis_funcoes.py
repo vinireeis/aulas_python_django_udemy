ab3cce87c8d61a1ce9d3f05ddf79a73004ebed35
Vinicius <vih-reis@hotmail.com>
2021-09-21 09:44:13 -0300

    aula parametros mutáveis

--- Aulas - módulo 3/a_parametros_mutaveis_funcoes.py
+++ Aulas - módulo 3/a_parametros_mutaveis_funcoes.py
@@ -1,13 +1,16 @@
 # objetos mutáveis, ex listas e dicionários
 # imutaveis, tuplas, strings, números, true, false, None etc.
 
-def lista_clientes(clientes_iteravel, lista=[]):
+def lista_clientes(clientes_iteravel, lista=None):
+    if lista is None:
+        lista = []
     lista.extend(clientes_iteravel)
     return lista
 
 
 clientes1 = lista_clientes(['João', 'Maria', 'Eduardo'])
 clientes2 = lista_clientes(['Marcos', 'Jonas', 'Zico'])
-
+clientes3 = lista_clientes(['José'])
 print(clientes1)
 print(clientes2)
+print(clientes3)
