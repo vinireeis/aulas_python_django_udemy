c7acb042ca9ae03b2c7932916b19843034cf1d75
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-09-23 12:49:58 -0300

    encaps

--- Módulo 4 - Orientacao a Objetos/a_encapsulamento.py
+++ Módulo 4 - Orientacao a Objetos/a_encapsulamento.py
@@ -33,8 +33,10 @@ bd.lista_clientes()
 bd.__dados = 'Outra coisa'
 # isso quebraria os métodos caso fosse público ou só com um..
 #  "underline" > self.dados ou self._dados
-# mas como definimos da forma mais "protegida" com __dados ele irá criar um novo atributo
-# e o atributo original será renomeado para _BaseDeDados__dados (_NomeDaClasse__nome_atributo)
+# mas como definimos da forma mais "protegida" com __dados ele irá criar..
+#  um novo atributo
+# e o atributo original será renomeado para _BaseDeDados__dados
+# O padrão é _NomeDaClasse__nome_atributo
 print(bd.__dados)
 print(bd._BaseDeDados__dados)
 # bd.lista_clientes() agora já não funciona mais por ter alterado a estrutura..
