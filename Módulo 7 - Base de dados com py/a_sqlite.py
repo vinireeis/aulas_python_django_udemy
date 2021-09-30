d5eaf60bc018c66582ce14c25b1f95a859619cc3
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-09-30 15:59:09 -0300

    sqlite py

---
+++ Módulo 7 - Base de dados com py/a_sqlite.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conexao = sqlite3.connect('basededados.db')
+cursor = conexao.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS clientes ('
+               'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+               'nome TEXT,'
+               'peso REAL'
+               ')')
+
+cursor.execute('INSERT INTO clientes (nome, peso) VALUES ("Vinicius", 77.5)')
+conexao.commit()
+cursor.execute('SELECT * FROM clientes')
+
+for linha in cursor.fetchall():
+    print(linha)
+
+cursor.close()
+conexao.close()
