4e8077c71cad680d4d16d92b9ad366477c127956
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-10-29 15:04:24 -0300

    fazendo a index 'homepage' como um app django

--- Módulo 8 - Django/Projeto 1 - Mywebsite/meusite/urls.py
+++ Módulo 8 - Django/Projeto 1 - Mywebsite/meusite/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from home import views
 
 
 urlpatterns = [
-    path('', views.index),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('sobre/', include('sobre.urls')),
+    path('', views.index,),
 ]
