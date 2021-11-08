d20cc2dd725dbfa5af7408da2963e6dcdd1cf251
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-11-08 11:15:38 -0300

    colocando os objetos criados no front

--- Módulo 8 - Django/Projeto 2 - Agenda/contatos/admin.py
+++ Módulo 8 - Django/Projeto 2 - Agenda/contatos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_diplay_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'data_criacao')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
