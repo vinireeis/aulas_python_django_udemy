c08086ec04d4cdf5d6a6ea4292f4a4034184647a
Vinicius Reis <v_reis@outlook.com.br>
2021-11-09 15:55:11 -0300

    upd

--- Módulo 8 - Django/Projeto 2 - Agenda/contatos/views.py
+++ Módulo 8 - Django/Projeto 2 - Agenda/contatos/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Contato
-from django.http import Http404
 from django.core.paginator import Paginator
+#  from django.http import Http404
 
 
 def index(request):
@@ -20,8 +20,6 @@ def index(request):
 #             })
 #     except Contato.DoesNotExist as e:
 #         raise Http404()
-
-
 def ver_contato(request, id_contato):
     contato = get_object_or_404(Contato, id=id_contato)
     return render(request, 'contatos/ver_contato.html', {
@@ -30,8 +28,18 @@ def ver_contato(request, id_contato):
 
 
 def busca(request):
-    contatos = Contato.objects.order_by('-id').filter(publicar=True)
+    termo = request.GET.get('termo')
+    contatos = Contato.objects.order_by('-id').filter(nome__icontains=termo,
+     sobrenome__icontains=termo,
+     publicar=True)
     paginator = Paginator(contatos, 3)
     page = request.GET.get('page')
     contacts = paginator.get_page(page)
     return render(request, 'contatos/busca.html', {'contatos': contacts})
+
+# def busca(request):
+#     contatos = Contato.objects.order_by('-id').filter(publicar=True)
+#     paginator = Paginator(contatos, 3)
+#     page = request.GET.get('page')
+#     contacts = paginator.get_page(page)
+#     return render(request, 'contatos/busca.html', {'contatos': contacts})
