ac13d31d34fba8b17c079846d2679f5ea0cd0bb5
Vinicius Reis <v_reis@outlook.com.br>
2021-11-11 16:00:15 -0300

    criando form com django

--- Módulo 8 - Django/Projeto 2 - Agenda/accounts/views.py
+++ Módulo 8 - Django/Projeto 2 - Agenda/accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.core.validators import validate_email
 from django.contrib.auth.decorators import login_required
-
+from .models import CadastrarContato
 
 def login(request):
     if request.method != 'POST':
@@ -73,6 +73,20 @@ def register(request):
     user.save()
     return redirect('login')
 
+
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = CadastrarContato()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = CadastrarContato(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, "Há dados incompletos")
+        form = CadastrarContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form.save()
+    messages.success(request, 'Contato salvo com sucesso')
+    return redirect('dashboard')
